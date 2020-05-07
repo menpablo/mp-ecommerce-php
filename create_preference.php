107e0a6715ce5dbf1ac01291f1f1780cefe1e7a2
pablo <menpablo@hotmail.com>
2020-05-07 11:01:28 -0300

    Creo Pref

---
+++ create_preference.php
@@ -0,0 +1,35 @@
+<?php
+
+use MercadoPago\Item;
+use MercadoPago\Preference;
+
+$page_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+$back_urls = array(
+    "success" => $page_url."/myContracts?paymentState=succeed",
+    "failure" => $page_url."/myContracts?paymentState=failed",
+    "pending" => $page_url."/myContracts?paymentState=pending",
+);
+
+$preference = createPreference("Test",500,"$$$",$back_urls);
+return ['url' => $preference->init_point ];
+
+function createPreference($descripcion,$price,$reference, $back_urls = []){
+    $preference = new Preference();
+
+    // Crea un ítem en la preferencia
+    $item = new Item();
+    $item->title = $descripcion;
+    $item->quantity = 1;
+    $item->unit_price = $price;
+    $item->currency_id = "ARS";
+
+    if(!empty($back_urls)){
+        $preference->back_urls = $back_urls;
+    }
+
+    $preference->items = array($item);
+    $preference->external_reference = $reference;
+    $preference->save();
+    return $preference;
+}
--- detail.php
+++ detail.php
@@ -113,25 +113,27 @@
                                     </div>
 
                                 </div>
-                                <div class="as-producttile-info" style="float:left;min-height: 168px;">
-                                    <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
-                                        <div class="as-producttile-title">
-                                            <h3 class="as-producttile-name">
-                                                <p class="as-producttile-tilelink">
-                                                    <span data-ase-truncate="2"><?php echo $_POST['title'] ?></span>
-                                                </p>
+                                <form action="/create_preference.php" method="get">
+                                    <div class="as-producttile-info" style="float:left;min-height: 168px;">
+                                        <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
+                                            <div class="as-producttile-title">
+                                                <h3 class="as-producttile-name">
+                                                    <p class="as-producttile-tilelink">
+                                                        <span data-ase-truncate="2"><?php echo $_POST['title'] ?></span>
+                                                    </p>
 
+                                                </h3>
+                                            </div>
+                                            <h3 >
+                                                <?php echo $_POST['price'] ?>
+                                            </h3>
+                                            <h3 >
+                                                <?php echo "$" . $_POST['unit'] ?>
                                             </h3>
                                         </div>
-                                        <h3 >
-                                            <?php echo $_POST['price'] ?>
-                                        </h3>
-                                        <h3 >
-                                            <?php echo "$" . $_POST['unit'] ?>
-                                        </h3>
+                                        <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
