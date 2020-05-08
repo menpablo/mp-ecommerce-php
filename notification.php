2024f7441440051fed69ca8624cae793dedeaa1a
pablo <menpablo@hotmail.com>
2020-05-08 03:14:46 -0300

    Preferencia creada de prueba

--- notification.php
+++ notification.php
@@ -1,11 +1,23 @@
 <?php
 
+use MercadoPago\SDK;
+
 require 'vendor/autoload.php';
 
-error_log("hello, this is a test!");
+SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-$request_body = file_get_contents('php://input');
-error_log($request_body);
+$merchant_order = null;
 
-error_log(json_encode($_REQUEST));
+switch($_GET["topic"]) {
+    case "payment":
+        $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+        // Get the payment and the corresponding merchant_order reported by the IPN.
+        $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
+        break;
+    case "merchant_order":
+        $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
+        break;
+}
+
+error_log(json_encode($merchant_order));
 
