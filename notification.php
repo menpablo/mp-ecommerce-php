738661d509c42d76fcc86b17133fdfb8a08a675e
pablo <menpablo@hotmail.com>
2020-05-08 03:01:31 -0300

    Preferencia creada de prueba

--- notification.php
+++ notification.php
@@ -4,4 +4,5 @@ require 'vendor/autoload.php';
 
 error_log("hello, this is a test!");
 
-error_log(json_encode($_POST));
+$request_body = file_get_contents('php://input');
+error_log(json_encode($request_body));
