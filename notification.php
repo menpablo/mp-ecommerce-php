ed436dea7767d349b698c38ca8d6c3caef07e09f
pablo <menpablo@hotmail.com>
2021-02-03 10:59:12 -0300

    Fixes

--- notification.php
+++ notification.php
@@ -8,7 +8,7 @@ SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a
 
 $merchant_order = null;
 
-switch($_GET["topic"]) {
+switch($_REQUEST["topic"]) {
     case "payment":
         $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
         // Get the payment and the corresponding merchant_order reported by the IPN.
@@ -21,3 +21,6 @@ switch($_GET["topic"]) {
 
 error_log("Webhook received ".json_encode($merchant_order));
 
+error_log("Webhook received 2 ".json_encode(file_get_contents('php://input')));
+
+var_dump($_REQUEST);
